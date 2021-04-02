83c0590763aa2aaebc500fb63961dd0015ed22fd
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2021-04-02 11:35:56 +0200

    revising implementation of real-time viewer

--- src/viewer.cpp
+++ src/viewer.cpp
@@ -3,40 +3,9 @@
 #define TINYPLY_IMPLEMENTATION
 #include "tinyply.h"
 
-#include "point.h"
 #include "viewer.h"
 
-void context(std::mutex& m, Kinect& kinect,
-    std::shared_ptr<std::vector<float>>& sptr_points,
-    std::shared_ptr<std::pair<Point, Point>>& sptr_threshold)
-{
-
-    Point min;
-    Point max;
-    min = sptr_threshold->first;
-    max = sptr_threshold->second;
-
-    int width = k4a_image_get_width_pixels(kinect.m_pclImage);
-    int height = k4a_image_get_height_pixels(kinect.m_pclImage);
-
-    for (int i = 0; i < width * height; i++) {
-        if ((*sptr_points)[3 * i + 0] > max.m_x
-            || (*sptr_points)[3 * i + 0] < min.m_x
-            || (*sptr_points)[3 * i + 1] > max.m_y
-            || (*sptr_points)[3 * i + 1] < min.m_y
-            || (*sptr_points)[3 * i + 2] > max.m_z
-            || (*sptr_points)[3 * i + 2] < min.m_z) {
-            (*sptr_points)[3 * i + 0] = 0.f;
-            (*sptr_points)[3 * i + 1] = 0.f;
-            (*sptr_points)[3 * i + 2] = 0.f;
-            continue;
-        }
-    }
-}
-
-void viewer::draw(std::mutex& m, Kinect kinect, int numPoints,
-    shared_ptr<std::vector<float>>& sptr_points,
-    std::shared_ptr<std::pair<Point, Point>>& sptr_threshold)
+void viewer::draw(Kinect& kinect)
 {
     /** create window and bind its context to the main thread */
     pangolin::CreateWindowAndBind("VIGITIA", 2560, 1080);
@@ -51,11 +20,11 @@ void viewer::draw(std::mutex& m, Kinect kinect, int numPoints,
     // pangolin::GetBoundWindow()->RemoveCurrent();
 
     /** create vertex and colour buffer objects and register them with CUDA */
-    pangolin::GlBuffer vA(
-        pangolin::GlArrayBuffer, numPoints, GL_FLOAT, 3, GL_STATIC_DRAW);
-    pangolin::GlBuffer cA(pangolin::GlArrayBuffer, numPoints, GL_UNSIGNED_BYTE,
-        3, GL_STATIC_DRAW);
-    std::vector<uint8_t> colours(numPoints * 3, 255);
+    pangolin::GlBuffer vA(pangolin::GlArrayBuffer, kinect.getNumPoints(),
+        GL_FLOAT, 3, GL_STATIC_DRAW);
+    pangolin::GlBuffer cA(pangolin::GlArrayBuffer, kinect.getNumPoints(),
+        GL_UNSIGNED_BYTE, 3, GL_STATIC_DRAW);
+    std::vector<uint8_t> colours(kinect.getNumPoints() * 3, 255);
 
     /** define camera render object for scene browsing */
     pangolin::OpenGlRenderState camera(
@@ -74,15 +43,13 @@ void viewer::draw(std::mutex& m, Kinect kinect, int numPoints,
     while (true) {
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        kinect.capture();
-        if (m.try_lock()) {
-            kinect.pclImage(sptr_points);
-            context(m, kinect, sptr_points, sptr_threshold);
-            vA.Upload(
-                (void*)sptr_points->data(), numPoints * 3 * sizeof(float));
-            cA.Upload((void*)colours.data(), numPoints * 3 * sizeof(uint8_t));
-            m.unlock();
-        }
+        kinect.getCapture();
+        kinect.getPclImage();
+        // kinect.updateContext();
+        vA.Upload((void*)kinect.getPcl().data(),
+            kinect.getNumPoints() * 3 * sizeof(float));
+        cA.Upload(
+            (void*)colours.data(), kinect.getNumPoints() * 3 * sizeof(uint8_t));
         viewPort.Activate(camera);
         glClearColor(0.0, 0.0, 0.3, 1.0);
 
