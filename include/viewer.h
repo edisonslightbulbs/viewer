f3d1956eaf45c01332a769d0249a7045659abe47
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2021-04-19 14:38:21 +0200

    declaring draw overload function

--- include/viewer.h
+++ include/viewer.h
@@ -1,6 +1,7 @@
 #ifndef RENDERER_H
 #define RENDERER_H
 
+#include <context.h>
 #include <memory>
 #include <mutex>
 #include <vector>
@@ -9,5 +10,6 @@
 
 namespace viewer {
 void draw(std::shared_ptr<Kinect>& kinect);
+void draw(Context* ptr_context, std::shared_ptr<Kinect>& sptr_kinect);
 }
 #endif /* RENDERER_H */
