6519889b673136dd057650f2b983dd1fcbd4518c
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2021-04-21 15:17:55 +0200

    checkpoint (initial implementation of segment context rendering)

--- include/viewer.h
+++ include/viewer.h
@@ -1,15 +1,15 @@
 #ifndef RENDERER_H
 #define RENDERER_H
 
-#include <context.h>
 #include <memory>
-#include <mutex>
 #include <vector>
 
+#include "intact.h"
 #include "kinect.h"
 
 namespace viewer {
 void draw(std::shared_ptr<Kinect>& kinect);
-void draw(Context* ptr_context, std::shared_ptr<Kinect>& sptr_kinect);
+void draw(
+    std::shared_ptr<Intact>& sptr_intact, std::shared_ptr<Kinect>& sptr_kinect);
 }
 #endif /* RENDERER_H */
