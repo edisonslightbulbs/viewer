75e3a8c81999e6721ac0c91abf8c49ff93bd0f0e
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2021-05-28 12:05:13 +0200

    revising code base

--- include/viewer.h
+++ include/viewer.h
@@ -4,10 +4,10 @@
 #include <memory>
 #include <vector>
 
-#include "intact.h"
+#include "i3d.h"
 #include "kinect.h"
 
 namespace viewer {
-void draw(std::shared_ptr<Intact>& sptr_i3d);
+void draw(std::shared_ptr<i3d>& sptr_i3d);
 }
 #endif /* RENDERER_H */
